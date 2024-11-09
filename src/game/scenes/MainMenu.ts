5b6a10b9ab7c2ec03bd177f3394d536cd42c0c68
UIT-24730009 <24730009@ms.hcmut.edu.vn>
2024-11-09 18:57:53 +0700

    Start game, update directions

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -4,40 +4,41 @@ import { EventBus } from "../EventBus";
 import Snake from "../models/Snake";
 
 export class MainMenu extends Scene {
-    background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
-    logoTween: Phaser.Tweens.Tween | null;
-    snake: Snake;
+  background: GameObjects.Image;
+  logo: GameObjects.Image;
+  title: GameObjects.Text;
+  logoTween: Phaser.Tweens.Tween | null;
+  snake: Snake;
 
-    constructor() {
-        super("MainMenu");
+  constructor() {
+    super("MainMenu");
+  }
+
+  preload() {
+    this.load.setPath("assets");
+    this.snake = new Snake(this);
+    this.snake.preload();
+  }
+
+  create() {
+    this.background = this.add.image(512, 384, "background");
+
+    this.snake.createAnimations();
+    this.snake.initializeSnake();
+
+    EventBus.emit("current-scene-ready", this);
+  }
+
+  update(time: number, delta: number): void {
+    this.snake.update(time);
+  }
+
+  changeScene() {
+    if (this.logoTween) {
+      this.logoTween.stop();
+      this.logoTween = null;
     }
 
-    preload() {
-        this.load.setPath('assets');
-        this.snake = new Snake(this);
-        this.snake.preload();
-    }
-
-    create() {
-
-        this.background = this.add.image(512, 384, "background");
-
-        this.snake.createAnimations();
-        this.snake.initializeSnake();
-
-        EventBus.emit("current-scene-ready", this);
-    }
-
-    changeScene() {
-        if (this.logoTween) {
-            this.logoTween.stop();
-            this.logoTween = null;
-        }
-
-        this.scene.start("Game");
-    }
-
+    this.scene.start("Game");
+  }
 }
-
